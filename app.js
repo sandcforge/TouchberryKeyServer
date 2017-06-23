38e537d07d2581ae3142e17d1602d745be45afdb
lc <lc@lcs-MacBook-Pro.local>
2017-06-22 20:59:37 -0700

    get rid of sessions

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var express = require('express');
-var session = require('express-session');
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var cookieParser = require('cookie-parser');
-var MongoStore = require('connect-mongo')(session);
 var envConfig = require('./config');
 
 var app = express();
@@ -66,16 +64,6 @@ else {
 
 
 console.log(envConfig.dbURL);
-
-
-app.use(session({
-	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
-	proxy: true,
-	resave: true,
-	saveUninitialized: true,
-	store: new MongoStore({ url: envConfig.dbURL })
-	})
-);
 
 require('./app/server/routes')(app);
 if ( process.env.NODE_ENV == 'prod') {
